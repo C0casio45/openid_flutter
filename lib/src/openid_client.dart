47d3597756a5c64c6718dc63490220f100c8d086
Tom <tom.lucas@viacesi.fr>
2022-03-28 21:54:30 +0200

    🔨 Client bases

---
+++ lib/src/openid_client.dart
@@ -0,0 +1,14 @@
+part of openid_flutter;
+
+class Client {
+  /// [clientId] is the id of the client
+  final String clientId;
+
+  /// [clientSecret] is the secret who permits to auth with the Openid Provider
+  final String? clientSecret;
+
+  /// The [Issuer] representing the OpenId Provider.
+  final Issuer issuer;
+
+  Client(this.issuer, this.clientId, this.clientSecret);
+}
