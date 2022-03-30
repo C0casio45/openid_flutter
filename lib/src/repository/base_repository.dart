4cfb58cf6e72aae38c8349c462b0fcbed048518d
Tom <tom.lucas@viacesi.fr>
2022-03-30 19:55:35 +0200

    🎨 implement base repository

---
+++ lib/src/repository/base_repository.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+class BaseRepository {
+  Future getHttp(Uri url,
+      {Map<String, String>? headers, required http.Client? client}) async {
+    return _processResponse(await _withClient(
+        (client) => client.get(url, headers: headers), client));
+  }
+
+  Future postHttp(Uri url,
+      {Map<String, String>? headers,
+      body,
+      Encoding? encoding,
+      required http.Client? client}) async {
+    return _processResponse(await _withClient(
+        (client) =>
+            client.post(url, headers: headers, body: body, encoding: encoding),
+        client));
+  }
+}
