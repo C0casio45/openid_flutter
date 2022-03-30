c8da7896a16d66126f9d2dc7f1d40a6726a49578
Tom <tom.lucas@viacesi.fr>
2022-03-30 19:56:09 +0200

    🔨 implement issuer discovery

--- lib/src/openid_issuer.dart
+++ lib/src/openid_issuer.dart
@@ -8,9 +8,27 @@ class Issuer {
 
   final JsonWebKeyStore _keyStore;
 
+  static final Map<Uri, Issuer?> _discoveries = {};
+
+  static Iterable<Uri> get knownIssuers => _discoveries.keys;
+
   /// Creates an issuer from its metadata.
   Issuer(this.metaDatas, {this.claimsMap = const {}})
       : _keyStore = metaDatas.jwksUri == null
             ? JsonWebKeyStore()
             : (JsonWebKeyStore()..addKeySetUrl(metaDatas.jwksUri!));
+
+  static Future<Issuer> discover(Uri uri, {Client? httpClient}) async {
+    if (_discoveries[uri] != null) return _discoveries[uri]!;
+
+    var segments = uri.pathSegments.toList();
+    if (segments.isNotEmpty && segments.last.isEmpty) {
+      segments.removeLast();
+    }
+    segments.addAll(['.well-known', 'openid-configuration']);
+    uri = uri.replace(pathSegments: segments);
+
+    var json = await http.get(uri, client: httpClient);
+    return _discoveries[uri] = Issuer(OpenIdProviderMetadata.fromJson(json));
+  }
 }
